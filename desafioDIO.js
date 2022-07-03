8783cc08221ed52896105293214d16478c40c463
jfbarkokebas <jfbarkokebas@gmail.com>
2022-07-03 01:56:42 -0300

    primeiro commit

---
+++ desafioDIO.js
@@ -0,0 +1,9 @@
+// comando básicos do terminal de comandos 
+
+/*
+- cd -> muda de diretório
+- pwd -> mostra o diretorio atual 
+- ls -> lista todos os arquivos do doretorio atual
+- git add -. adiciona um diretorio para a area de stage
+
+*/
